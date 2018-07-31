c7d135ab487cdaa6bd265914aafa7e5d254f5992
FrantaF <frantafriedl@seznam.cz>
2018-07-31 18:00:15 +0800

    registration and login completed

--- app.rb
+++ app.rb
@@ -3,35 +3,49 @@ require 'sinatra/base'
 set :run, true
 
 # class App < Sinatra::Base
-   # Home  page
-   get "/" do
-    # @name = "Bob Smith"
+
+  # Login page
+  get "/" do    
+    erb :"login"
+  end
+
+  get "/login" do    
+    erb :"login"
+  end
+
+  post '/login' do        
+    p params[:user]
+    if User.find_by(email: params[:user][:email]).try(:authenticate, params[:user][:password]) == false 
+      redirect '/login'
+    else
+      redirect '/profile'      
+    end
+  end
+
+  #registration page
+  get "/registration" do    
     erb :"registration"
   end
 
-  get "/" do
-    # @name = "Bob Smith"
-    erb :"registration"
+
+  post '/register' do
+    user = User.new(params[:user])     
+    user.save
+    #user = User.new(:name => "FINAL USER", :email => "email", :password => "password", :password_digest => "password")
+    if user.save
+      redirect '/login' 
+    else
+      redirect '/registration'
+    end  
+
   end
 
-   #handle login form submision 
-   post '/signup' do
-     # user = User.new(params[:user])
-    # if user.save
-    # what should happen if the user is saved?
- # else
-    # what should happen if the user keyed in invalid date?
- # end
- redirect '/profile'
-
-end  
-   #user profile page
-   get "/profile" do
-    @name = "Bob Smith"
+  #Profile page
+  get "/profile" do    
+    @display = User.last.email
     erb :"profile"
   end
 
 
-
-# end
+  
 
--- models/user.rb
+++ models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
+   validates :name, presence: true
+   validates :email, presence: true
+   validates :email, uniqueness: true
+   validates :password, presence: true
+   
    has_secure_password
    has_many :votes
    has_many :questions
    has_many :answers
 
 end
+
