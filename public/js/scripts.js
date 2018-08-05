4aba56256f7b0470e3da80e8553021a854e8f01c
FrantaF <frantafriedl@seznam.cz>
2018-08-05 12:23:38 +0800

    show/hide answers and answer box. Implemented in vanilla JS

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,62 +1,13 @@
-//Set nav links to active/not active
-function changeActive(link){    
-	//deactivate all links
-	document.getElementById("nav-link1").classList.remove("active");
-	document.getElementById("nav-link2").classList.remove("active");
-	document.getElementById("nav-link3").classList.remove("active");
-	//set item's class to active	
-	link.className += " active";
+
+function changeDivDisplay(sender){
+   let id = document.getElementById(sender.id);
+   if (id==="show-answers"){
+      document.getElementById("show-answers").style.visibility = "visible"
+      document.getElementById("show-answers").style.id = "hide-answers"
+
+   } else{
+      document.getElementById("hide-answers").style.visibility = "hidden"
+      document.getElementById("hide-answers").style.id = "show-answers"
+   }
+
 }
-
-// Show/Hide upload files seciton
-function showUploadArea(){
-   document.getElementById("upload-files-area").style.display = "block";
-   document.getElementById("main-content").style.display = "none";  
-}
-
-function hideUploadArea(){
-   document.getElementById("upload-files-area").style.display = "none";  
-   document.getElementById("main-content").style.display = "block";  
-}
-
-
-
-// Drag and drop upload file
-// + function($) {
-//   'use strict';
-
-    // UPLOAD CLASS DEFINITION
-    // ======================
-
- //    var dropZone = document.getElementById('drop-zone');
- //    var uploadForm = document.getElementById('js-upload-form');
-
- //    var startUpload = function(files) {
- //       console.log(files)
- //    }
-
- //    uploadForm.addEventListener('submit', function(e) {
- //       var uploadFiles = document.getElementById('js-upload-files').files;
- //       e.preventDefault()
-
- //       startUpload(uploadFiles)
- //    })
-
- //    dropZone.ondrop = function(e) {
- //       e.preventDefault();
- //       this.className = 'upload-drop-zone';
-
- //       startUpload(e.dataTransfer.files)
- //    }
-
- //    dropZone.ondragover = function() {
- //       this.className = 'upload-drop-zone drop';
- //       return false;
- //    }
-
- //    dropZone.ondragleave = function() {
- //       this.className = 'upload-drop-zone';
- //       return false;
- //    }
-
- // }(jQuery);
